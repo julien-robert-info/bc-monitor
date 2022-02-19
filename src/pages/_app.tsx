313352c34dd61d2c74f96189d9657562d5399e1b
Julien ROBERT <julien.robert.info@gmail.com>
2022-02-19 15:02:26 +0100

    implement Material ui with clean homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import createEmotionCache from 'styles/createEmotionCache'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache()
+
+const App: React.FC<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>Blockchain monitor</title>
+        <meta
+          name="description"
+          content="Data mining tool for evm compatible blockchain"
+        />
+      </Head>
+      <CacheProvider value={emotionCache}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </CacheProvider>
+    </>
+  )
+}
+
+export default App
